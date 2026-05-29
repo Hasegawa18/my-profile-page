2ab890949e6279662f977366214542f1a3a81643
hasegawa kenta <hasegawa-kenta-wt@ynu.jp>
2026-05-30 00:16:32 +0900

    layoutsを追加、first commit

---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+<script></script>
+
+<template>
+  <header>
+    <ul class="header-contents">
+      <li><a href="#">NEWS</a></li>
+      <li><a href="#">プロフィール</a></li>
+      <li><a href="#">SNS</a></li>
+      <li><a href="#">質問はこちらから！</a></li>
+    </ul>
+  </header>
+</template>
+
+<style>
+li{
+  list-style:none;
+}
+
+ul{
+  padding-left: 0;
+}
+
+.header-contents{
+  display: flex;
+}
+</style>
