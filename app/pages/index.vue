d2b0631f689b62377bb476960b85ae13583511df
hasegawa kenta <hasegawa-kenta-wt@ynu.jp>
2026-05-31 23:25:29 +0900

    swiperを追加

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,8 +1,41 @@
-<script setup lang="ts">
+<script setup>
+import { Swiper, SwiperSlide } from 'swiper/vue'
+
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+
+import { Navigation, Pagination } from 'swiper/modules'
 </script>
+
 
 <template>
   <div>
     ようこそ！
   </div>
+
+  <Swiper
+    :modules="[Navigation, Pagination]"
+    :slides-per-view="1"
+    :loop="true"
+    :navigation="true"
+    :pagination="{ clickable: true }"
+  >
+    <SwiperSlide><img src="/images/decoration.jpeg" alt="decoration"></SwiperSlide>
+    <SwiperSlide><img src="/images/monument.jpeg" alt="monument"></SwiperSlide>
+    <SwiperSlide><img src="/images/yaon.jpeg" alt="yaon"></SwiperSlide>
+  </Swiper>
 </template>
+
+<style scoped>
+.swiper {
+  width: 100%;
+  height: 300px;
+}
+
+.swiper-slide {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
